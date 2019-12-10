5479068aa1db500473e59db71aa6ab95f354a7e8
Souha-Bedoui <36705815+Souha-Bedoui@users.noreply.github.com>
2019-12-10 11:09:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/guidekelibia/DashboardActivity.java
@@ -0,0 +1,18 @@
+package com.example.guidekelibia;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DashboardActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/guidekelibia/page2.java
@@ -0,0 +1,13 @@
+package com.example.guidekelibia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page2);
+    }}
